c60944b256238de038399819ed8241020896eaab
Spok0jny <rbasta100@gmail.com>
2026-08-05 17:11:40 +0200

    CssSelector & xpath

---
+++ SeleniumKurs2/SeleniumKurs2/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumKurs2;
+
+public class Tests
+{
+    private IWebDriver _driver;
+    [SetUp]
+    public void Setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Manage().Window.Maximize();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        _driver.Navigate().GoToUrl("https://github.com/login");
+        _driver.FindElement(By.Id("login_field")).SendKeys("testingseleniumwebdriver");
+        _driver.FindElement(By.Id("password")).SendKeys("secret1234");
+        // _driver.FindElement(By.CssSelector("input[value='Sign in']")).Click();
+
+        _driver.FindElement(By.XPath("//input[@value='Sign in']"));
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+        _driver.Dispose();
+    }
+}
