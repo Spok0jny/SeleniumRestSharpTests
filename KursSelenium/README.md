c1d3a7facc19b66f81c86e02471957321100ff03
Spok0jny <rbasta100@gmail.com>
2026-08-05 13:50:27 +0200

    Selenium kurs 1

--- KursSelenium/README.md
+++
@@ -1,3 +0,0 @@
-# KursSelenium
-
-This project is an introductory C# console application designed as an entry point for learning Selenium WebDriver automation concepts. It serves as a foundational template for experimenting with basic C# syntax and initial test setups.
---
+++ SeleniumKurs1/SeleniumKurs1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumKurs1;
+
+public class Tests
+{
+    private IWebDriver _driver;
+
+    [SetUp]
+    public void Setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Manage().Window.Maximize();
+    }
+
+    [Test]
+    public void EnterGitHub()
+    {
+        _driver.Navigate().GoToUrl("https://github.com");
+        Assert.That(_driver.Title, Does.Contain("GitHub"));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+        _driver.Dispose();
+        
+        
+    }
+}
