9461d075abe504847e435883028f7f9126dc5ef4
Spokojny <rbasta100@gmail.com>
2026-08-07 23:00:01 +0200

    Module10

---
+++ Module10/Module10/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Module10
+{
+    public class Tests
+    {
+        private IWebDriver _driver;
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+          
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+            _driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+            IWebElement loginButton = _driver.FindElement(By.CssSelector("button[type='submit']"));
+
+            js.ExecuteScript("arguments[0].click()", loginButton);
+
+            var pageTitle = js.ExecuteScript("return document.title;");
+           
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _driver.Dispose();
+        }
+    }
+}
